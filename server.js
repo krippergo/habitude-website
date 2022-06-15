7eb3e0598b908823b396d0ffa9cb6e4c80c56a4d
vladimir.klimov <vladimirklimov2008@gmail.com>
2022-06-15 18:53:26 +0400

    начал делать download

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require('express'), app = express();
 
-app.get('/download', function (request, response) {
+app.get('/', function (request, response) {
     response.sendFile(__dirname + '/src/public/download.html');
 });
 
-app.get('/css/download', function (request, response) {
+app.get('/css', function (request, response) {
     response.sendFile(__dirname + '/src/style/css/download.css');
 });
 
-app.get('/js/download', function (request, response) {
+app.get('/js', function (request, response) {
     response.sendFile(__dirname + '/src/app/download.js');
 });
 
-//app.get('/img', function (request, response) {
-//    response.sendFile(__dirname + '/src/images/icon.png');
-//});
+app.get('/img', function (request, response) {
+    response.sendFile(__dirname + '/src/images/icon.png');
+});
+
+app.get('/img/menu', function (request, response) {
+    response.sendFile(__dirname + '/src/images/menu.png');
+});
+
+app.get('/img/background', function (request, response) {
+    response.sendFile(__dirname + '/src/images/bg.png');
+});
 
 app.set('port', (process.env.PORT || 80));
 
