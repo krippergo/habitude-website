1f61024a2e5bfbbbff7bb4bba60d9503f0c6e5d7
vladimir.klimov <vladimirklimov2008@gmail.com>
2022-06-15 16:36:39 +0400

    init

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express'), app = express();
+
+app.get('/download', function (request, response) {
+    response.sendFile(__dirname + '/src/public/download.html');
+});
+
+app.get('/css/download', function (request, response) {
+    response.sendFile(__dirname + '/src/style/css/download.css');
+});
+
+app.get('/js/download', function (request, response) {
+    response.sendFile(__dirname + '/src/app/download.js');
+});
+
+//app.get('/img', function (request, response) {
+//    response.sendFile(__dirname + '/src/images/icon.png');
+//});
+
+app.set('port', (process.env.PORT || 80));
+
+app.listen(app.get('port'), function() {
+	console.log('Порт', app.get('port'), 'запущен');
+});
