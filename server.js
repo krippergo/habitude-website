4f5a0bd58b7e47ba1e52d98d87112ce09ca34dbc
vladimir.klimov <vladimirklimov2008@gmail.com>
2022-06-21 23:06:00 +0400

    авторизация и регистрация

--- server.js
+++ server.js
@@ -1,4 +1,104 @@
 const express = require('express'), app = express();
+const session = require('express-session');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const md5 = require('md5');
+const moment = require('moment');
+
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+
+const path = './users.txt';
+
+fs.access(path, fs.F_OK, (err) => {
+    if (err) {
+        var filepath = "users.txt";
+        fs.open(filepath, 'w', (err) => {
+            if (err) throw err;
+            console.log("Файл создан");
+            readFile()
+        });
+    } else {
+        console.log("Файл есть");
+        readFile()
+    }
+});
+
+var users;
+
+function readFile() {
+    fs.readFile("users.txt", "utf8", function (error, data) {
+        if (data) {
+            users = JSON.parse(data);
+        } else {
+            users = [];
+        }
+    });
+}
+
+function yearCheck(year) {
+    var leap = false;
+    if (year % 400 == 0) {
+        leap = true;
+    } else if (year % 100 == 0) {
+        leap = false;
+    } else if (year % 4 == 0) {
+        leap = true;
+    } else {
+        leap = false;
+    }
+    return leap;
+}
+
+function plusOneYear(year, mon, day, hoursMinutesSeconds) {
+    var year = Number(moment().format("YYYY"));
+    var leapYear = yearCheck(year);
+    var mon = Number(moment().format("M"));
+    var checForMon;
+    if (mon == 4 || mon == 6 || mon == 9 || mon == 11) {
+        checForMon = 1;
+    } else {
+        checForMon = 2;
+    }
+    var day = Number(moment().format("D"));
+    if (mon == 12 && day == 31) {
+        day = 1;
+        mon = 1;
+        year = year + 1;
+    } else if (checForMon == 1 && day < 31) {
+        day = day + 1;
+    } else if (checForMon == 1 && day == 31) {
+        day = 1;
+        mon = mon + 1;
+    } else if (mon == 2 && day < 29 && leapYear) {
+        day = day + 1;
+    } else if (mon == 2 && day == 29) {
+        day = 1;
+        mon = mon + 1;
+    } else if (mon == 2 && day == 28 && !leapYear) {
+        day = 1;
+        mon = mon + 1;
+    } else if (mon == 2 && day < 28) {
+        day = day + 1;
+    } else if (checForMon == 2 && day == 30) {
+        day = 1;
+        mon = mon + 1;
+    } else {
+        day = day + 1;
+    }
+    var hoursMinutesSeconds = moment().format("HH:mm:ss");
+    var realDataPlusDay = `${year}.${mon}.${day} : ${hoursMinutesSeconds}`;
+    return realDataPlusDay;
+}
+
+app.use(bodyParser.json());
+app.use(session({
+    secret: "21d6f40cfb511982e4424e0e250a9557",
+    saveUninitialized: false,
+    cookie: {
+        originalMaxAge: 8640000,
+        httpOnly: true
+    }
+}));
 
 app.get('/', function (request, response) {
     response.sendFile(__dirname + '/src/public/download.html');
@@ -28,6 +128,10 @@ app.get('/img/telegram', function (request, response) {
     response.sendFile(__dirname + '/src/images/telegram.png');
 });
 
+app.get('/img/exit', function (request, response) {
+    response.sendFile(__dirname + '/src/images/exit.png');
+});
+
 app.get('/img/background', function (request, response) {
     response.sendFile(__dirname + '/src/images/bg.png');
 });
@@ -39,6 +143,93 @@ app.get('/files/apk/habitude', function (request, response) {
 app.get('/fonts/inter', function (request, response) {
     response.sendFile(__dirname + '/src/fonts/inter.ttf');
 });
+
+app.post('/registration', urlencodedParser, function (request, response, data) {
+    fs.readFile("users.txt", "utf8", function (error, data) {
+        if (data) {
+            users = JSON.parse(data);
+        } else {
+            users = [];
+        }
+    });
+
+    var userRegistration = false;
+    for (a = 0; a < users.length; a++) {
+        var login_file = users[a].login;
+        if (login_file === request.body.login) {
+            userRegistration = true;
+            break;
+        }
+    }
+    var obj;
+    if (userRegistration) {
+        obj = {
+            verification: "false"
+        }
+        response.json(obj);
+    } else {
+        request.body.password = md5(request.body.password);
+        users.push(request.body);
+        var fileContent = JSON.stringify(users);
+        fs.writeFileSync('users.txt', fileContent);
+        request.session.login = request.body.login;
+        var year = Number(moment().format("YYYY"));
+        var mon = Number(moment().format("M"));
+        var day = Number(moment().format("D"));
+        var hoursMinutesSeconds = moment().format("HH:mm:ss");
+        var time = `${year}.${mon}.${day} : ${hoursMinutesSeconds}`;
+        var endData = plusOneYear(year, mon, day, hoursMinutesSeconds);
+        request.session.dataStart = time;
+        request.session.dataEnd = endData;
+        obj = {
+            verification: "true"
+        }
+        response.json(obj);
+    }
+});
+
+app.post('/authorization', urlencodedParser, function (request, response, data) {
+    fs.readFile("users.txt", "utf8", function (error, data) {
+        if (data) {
+            users = JSON.parse(data);
+        } else {
+            users = [];
+        }
+    });
+
+    var userLogin = false;
+    for (a = 0; a < users.length; a++) {
+        var login_file = users[a].login;
+        var pass_file = users[a].password;
+        if (login_file === request.body.login && pass_file === md5(request.body.password)) {
+            userLogin = true;
+            break;
+        }
+    }
+    var obj;
+    if (userLogin) {
+        request.session.login = request.body.login;
+        var year = Number(moment().format("YYYY"));
+        var mon = Number(moment().format("M"));
+        var day = Number(moment().format("D"));
+        var hoursMinutesSeconds = moment().format("HH:mm:ss");
+        var time = `${year}.${mon}.${day} : ${hoursMinutesSeconds}`;
+        var endData = plusOneYear(year, mon, day, hoursMinutesSeconds);
+        request.session.dataStart = time;
+        request.session.dataEnd = endData;
+        obj = {
+            verification: "true"
+        }
+        response.json(obj);
+    } else {
+        obj = {
+            verification: "false"
+        }
+        response.json(obj);
+    }
+});
+
+app.get('/profile', function (request, response) { });
 
 app.set('port', (process.env.PORT || 80));
 
--- src/app/download.js
+++ src/app/download.js
@@ -17,9 +17,46 @@ function createMenu() {
 	var imgMenu = document.getElementById("img-menu");
 	imgMenu.style.transform = "rotate(1rad)";
 	var menu = document.getElementById("menu");
-	menu.style.transform = "translationX(0%)";
+	menu.style.left = "0px";
+}
+
+function goToHome() {
+	document.location.href = "/";
+}
+
+function goToReg() {
+	deleteMenu();
 	var bgMenu = document.getElementById("bg-menu");
-	bgMenu.style.display = "block";
+	bgMenu.style.display = "flex";
+	var regist = document.getElementById("registration");
+	regist.style.display = "block";
+	var log = document.getElementById("log-in");
+	log.style.display = "none";
+	var title = document.getElementById("title");
+	title.innerHTML = "Habitude: registration";
+}
+
+function goToLogin() {
+	deleteMenu();
+	var bgMenu = document.getElementById("bg-menu");
+	bgMenu.style.display = "flex";
+	var log = document.getElementById("log-in");
+	log.style.display = "block";
+	var regist = document.getElementById("registration");
+	regist.style.display = "none";
+	var title = document.getElementById("title");
+	title.innerHTML = "Habitude: authorization";
+}
+
+function exit() {
+	var bgMenu = document.getElementById("bg-menu");
+	bgMenu.style.display = "none";
+	var log = document.getElementById("log-in");
+	log.style.display = "none";
+	var regist = document.getElementById("registration");
+	regist.style.display = "none";
+	var title = document.getElementById("title");
+	title.innerHTML = "Habitude: download";
 }
 
 function createContactsMenu() {
@@ -32,12 +69,86 @@ function deleteContactsMenu() {
 	containerContacts.style.display = "none";
 }
 
+function goToVersions() {
+	document.location.href = "/versions";
+}
+
 function deleteMenu() {
 	var menu = document.getElementById("menu");
-	menu.style.transform = "translationX(-100%)";
-	var bgMenu = document.getElementById("bg-menu");
-	bgMenu.style.display = "none";
+	menu.style.left = "-360px";
 	var imgMenu = document.getElementById("img-menu");
 	imgMenu.style.transform = "rotate(0rad)";
 	imgMenu.style.transition = "All 0.5s ease";
+}
+
+function verification() {
+	var logSelector = document.getElementById("login");
+	var passSelector = document.getElementById("password");
+	var cpassSelector = document.getElementById("confirm");
+	var textErrorSelector = document.getElementById("text-error");
+	var log = document.getElementById("login").value;
+	var pass = document.getElementById("password").value;
+	var cpass = document.getElementById("confirm").value;
+
+	if (log != "") {
+		if (pass != "") {
+			if (pass == cpass) {
+				var submitData = {
+					login: log,
+					password: pass
+				};
+				console.log(submitData);
+
+				var xhttp = new XMLHttpRequest();
+				xhttp.open("POST", "/registration", true);
+				xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+				xhttp.send(JSON.stringify(submitData));
+				xhttp.onload = function () {
+					if (this.readyState == 4 && this.status == 200) {
+						var json = JSON.parse(this.responseText);
+						if (json.verification == "true") {
+							document.location.href = "/profile";
+						} else {
+							logSelector.style.borderColor = "#45A29E";
+							textErrorSelector.innerHTML = "введите другой логин";
+						}
+					}
+				};
+			} else {
+				cpassSelector.style.borderColor = "#45A29E";
+				textErrorSelector.innerHTML = "вы не подтвердили пароль";
+			}
+		} else {
+			passSelector.style.borderColor = "#45A29E";
+			textErrorSelector.innerHTML = "введите пароль";
+		}
+	} else {
+		logSelector.style.borderColor = "#45A29E";
+		textErrorSelector.innerHTML = "введите логин";
+	}
+}
+
+function verificationLogin() {
+	var textErrorSelector = document.getElementById("text-error-sign-in");
+	var log = document.getElementById("login-sign-in").value;
+	var pass = document.getElementById("password-sign-in").value;
+	var submitData = {
+		login: log,
+		password: pass
+	};
+	console.log(submitData);
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("POST", "/authorization", true);
+	xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+	xhttp.send(JSON.stringify(submitData));
+	xhttp.onload = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			var json = JSON.parse(this.responseText);
+			if (json.verification == "true") {
+				document.location.href = "/profile";
+			} else {
+				textErrorSelector.innerHTML = "логин/пароль неверный";
+			}
+		}
+	};
 }
